4ae0a80134aa54f1f02af721461318cf03cb8f13
Greg Tebbutt <greg@sparrho.com>
2015-11-17 16:32:40 +0000

    Remove unused setting

--- src/demeter/settings.cljs
+++ src/demeter/settings.cljs
@@ -1,7 +1,6 @@
 (ns demeter.settings
   (:require [demeter.util :refer [env parse-int]]))
 
-(def s3-bucket (env "S3_BUCKET"))
 (def prerender-port (env "PRERENDER_PORT" 4000))
 (def log-level (env "LOG_LEVEL" "info"))
 (def default-timeout (parse-int (env "DEFAULT_TIMEOUT" 5000)))
