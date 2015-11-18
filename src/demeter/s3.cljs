f23aace2dc15ba85ac0cc3550975d574d0caf7da
Greg Tebbutt <greg@sparrho.com>
2015-11-18 11:25:38 +0000

    AWS backup utilities

---
+++ src/demeter/s3.cljs
@@ -0,0 +1,34 @@
+(ns demeter.s3
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs.nodejs :as nodejs]
+            [cljs.core.async :refer [>! <! chan close!]]
+            [demeter.logging :as log]
+            [demeter.core :as core]))
+
+(def aws (nodejs/require "aws-sdk"))
+
+(def s3-constructor (.-S3 aws))
+(def s3 (s3-constructor.))
+
+(defn write
+  [body filename bucket & [public?]]
+  (let [channel (chan)
+        params {:Bucket bucket
+                :Key filename
+                :Body body}]
+    (.upload s3 (clj->js (if public? (assoc params :ACL "public-read") params))
+             (fn [err data]
+               (when err (log/error "S3 error: " err))
+               (go
+                (>! channel {:error err
+                             :data (js->clj data :keywordize-keys true)})
+                (close! channel))))
+    channel))
+
+(defn save-image
+  [url filename bucket]
+  (go
+   (if-let [body (<! (core/get-image url))]
+     (<! (write body filename bucket :public))
+
+     {:error :request-failed})))
